963ce7f7c19a6c88faa55faf0d9e9dd71579afb2
WebEpic <randy@randyjmitchell.com>
2015-06-09 09:59:36 -0400

    added index page and route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
+  
+  def index
+  	page = params[:page] || 1
+  	page_offset = (page - 1) * 10
+  	@posts = Post.order(written_at: :desc).offset(page_offset).limit(10)
+  end
+
   def show
     @post = Post.find(params[:id])
   end
